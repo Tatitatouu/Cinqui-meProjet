41246219dc41f76798290508043f25eedc9f44db
Tatitatouu <tatiana68270wi@gmail.com>
2024-09-04 15:19:44 +0200

    arrow-right and -left

--- Print-it-JS/assets/script.js
+++ Print-it-JS/assets/script.js
@@ -17,5 +17,40 @@ const slides = [
 	}
 ]
 
-maBaliseDiv=document.getElementById("banner")
+
+
+index=0;
+
+console.log(slides[index]);
+
+arrow_left=document.getElementById("arrow_left");
+function changeimageleft() {
+	index=(index - 1 + 4) % 4;
+
+	slide_image=slides[index].image;	
+	
+	banner=document.getElementById("banner-id");
+	banner.src="./assets/images/slideshow/" + slide_image;	
+
+	balise_p=document.getElementById("tagLine");
+	tagLine_p=slides[index].tagLine;
+	balise_p.innerHTML=tagLine_p;
+}
+arrow_left.addEventListener("click", changeimageleft);
+
+arrow_right=document.getElementById("arrow_right");
+function changeimageright() {
+	index=(index +1 ) % 4;
+	console.log(index);
+	
+	slide_image=slides[index].image;
+	
+	banner=document.getElementById("banner-id");
+	banner.src="./assets/images/slideshow/" + slide_image;	
+
+	balise_p=document.getElementById("tagLine");
+	tagLine_p=slides[index].tagLine;
+	balise_p.innerHTML=tagLine_p;
+}
+arrow_right.addEventListener("click", changeimageright);
 
