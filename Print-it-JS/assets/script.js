142bbe848a23e0cac319145ead3265a626c7997c
Tatitatouu <tatiana68270wi@gmail.com>
2024-09-05 10:43:25 +0200

    Je pense avoir fini

--- Print-it-JS/assets/script.js
+++ Print-it-JS/assets/script.js
@@ -1,56 +1,89 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+    {
+        "image":"slide1.jpg",
+        "tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image":"slide2.jpg",
+        "tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image":"slide3.jpg",
+        "tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image":"slide4.png",
+        "tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+    }
 ]
 
+index = 0;
 
+var dotsContainer = document.querySelector('.dots');
 
-index=0;
+function createDots() {
+    var dot1 = document.createElement('div');
+    var dot2 = document.createElement('div');
+    var dot3 = document.createElement('div');
+    var dot4 = document.createElement('div');
+
+    dot1.className = 'dot dot_selected';
+    dot2.className = 'dot';
+    dot3.className = 'dot';
+    dot4.className = 'dot'; 
+
+    dotsContainer.appendChild(dot1);
+    dotsContainer.appendChild(dot2);
+    dotsContainer.appendChild(dot3);
+    dotsContainer.appendChild(dot4);
+}
+
+function updateDots() {
+    var dots = document.querySelectorAll('.dot');
+
+    dots[0].classList.remove('dot_selected');
+    dots[1].classList.remove('dot_selected');
+    dots[2].classList.remove('dot_selected');
+    dots[3].classList.remove('dot_selected');
+
+    dots[index].classList.add('dot_selected'); 
+}
 
 console.log(slides[index]);
 
-arrow_left=document.getElementById("arrow_left");
+arrow_left = document.getElementById("arrow_left");
 function changeimageleft() {
-	index=(index - 1 + 4) % 4;
+    index = (index - 1 + 4) % 4;
 
-	slide_image=slides[index].image;	
-	
-	banner=document.getElementById("banner-id");
-	banner.src="./assets/images/slideshow/" + slide_image;	
+    var slide_image = slides[index].image;
 
-	balise_p=document.getElementById("tagLine");
-	tagLine_p=slides[index].tagLine;
-	balise_p.innerHTML=tagLine_p;
+    var banner = document.getElementById("banner-id");
+    banner.src = "./assets/images/slideshow/" + slide_image;
+
+    var balise_p = document.getElementById("tagLine");
+    var tagLine_p = slides[index].tagLine;
+    balise_p.innerHTML = tagLine_p;
+
+    updateDots(); 
 }
 arrow_left.addEventListener("click", changeimageleft);
 
-arrow_right=document.getElementById("arrow_right");
+arrow_right = document.getElementById("arrow_right");
 function changeimageright() {
-	index=(index +1 ) % 4;
-	console.log(index);
-	
-	slide_image=slides[index].image;
-	
-	banner=document.getElementById("banner-id");
-	banner.src="./assets/images/slideshow/" + slide_image;	
+    index = (index + 1) % 4;
+    console.log(index);
 
-	balise_p=document.getElementById("tagLine");
-	tagLine_p=slides[index].tagLine;
-	balise_p.innerHTML=tagLine_p;
+    var slide_image = slides[index].image;
+
+    var banner = document.getElementById("banner-id");
+    banner.src = "./assets/images/slideshow/" + slide_image;
+
+    var balise_p = document.getElementById("tagLine");
+    var tagLine_p = slides[index].tagLine;
+    balise_p.innerHTML = tagLine_p;
+
+    updateDots(); 
 }
 arrow_right.addEventListener("click", changeimageright);
 
+createDots();
